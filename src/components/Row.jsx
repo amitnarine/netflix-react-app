a770963a8d76e7d947fe557ec14cb42c98fff886
amitnarine <57597739+amitnarine@users.noreply.github.com>
2022-10-08 14:52:09 -0400

    added heart and created movie component

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
 const Row = ({ title, fetchURL }) => {
+  const [like, setLike] = useState(false);
   const [movies, setMovies] = useState([]);
+
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
       setMovies(response.data.results);
@@ -25,6 +28,13 @@ const Row = ({ title, fetchURL }) => {
               <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
                 <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
                   {item?.title}
+                </p>
+                <p>
+                  {like ? (
+                    <FaHeart className="absolute top-4 left-4 text-gray-300" />
+                  ) : (
+                    <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
+                  )}
                 </p>
               </div>
             </div>
