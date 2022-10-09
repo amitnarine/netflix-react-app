f221730d489577b8adede1e6a48aa907bf4594da
amitnarine <57597739+amitnarine@users.noreply.github.com>
2022-10-09 15:52:25 -0400

    added firebase configs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
 
 
 function App() {
   return (
     <>
+    <AuthContextProvider>
     <Navbar />
     <Routes>
       <Route path='/' element={<Home />} />
 
     </Routes>
+
+    </AuthContextProvider>
     </>
   );
 }
