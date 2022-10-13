cef58c6b81be23ebcf3522aee5a688818bedfe04
amitnarine <57597739+amitnarine@users.noreply.github.com>
2022-10-13 13:43:28 -0400

    creating layout for account

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          className="w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/afc06103-4d6a-4236-b496-34b671a7e9ba/0a1bb975-77bd-4c7e-8524-7fb05c9dd015/US-en-20221003-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="/"
+        ></img>
+
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]">
+          <div className="absolute top-[20%] p-4 md:p-8">
+            <h1 className="text-3xl md:5xl font-bold">My Shows</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Account;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
         <img
           className="hidden sm:block absolute w-full h-full object-cover"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/afc06103-4d6a-4236-b496-34b671a7e9ba/0a1bb975-77bd-4c7e-8524-7fb05c9dd015/US-en-20221003-popsignuptwoweeks-perspective_alpha_website_large.jpg"
-          alt=""
+          alt="/"
         ></img>
         <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
         <div className="fixed w-full px-4 py-24 z-50">
