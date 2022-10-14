61eb12d7f3665529a95582cf4a8efc6198cff8b4
amitnarine <57597739+amitnarine@users.noreply.github.com>
2022-10-14 14:10:49 -0400

    added delete option

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -3,6 +3,7 @@ import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { UserAuth } from "../context/AuthContext";
 import { db } from "../firebase";
 import { updateDoc, doc, onSnapshot } from "firebase/firestore";
+import { AiOutlineClose } from "react-icons/ai";
 
 const SavedShows = () => {
   const [movies, setMovies] = useState([]);
@@ -24,6 +25,17 @@ const SavedShows = () => {
       setMovies(doc.data()?.savedShows);
     });
   }, [user?.email]);
+
+  const movieRef = doc(db, "users", `${user?.email}`);
+
+  const deleteShow = async (passedID) => {
+    try {
+      const result = movies.filter((item) => item.id !== passedID);
+      await updateDoc(movieRef, { savedShows: result });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
@@ -51,6 +63,12 @@ const SavedShows = () => {
               <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
                 <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
                   {item?.title}
+                </p>
+                <p
+                  onClick={() => deleteShow(item.id)}
+                  className="absolute text-gray-300 top-4 right-4"
+                >
+                  <AiOutlineClose />
                 </p>
               </div>
             </div>
